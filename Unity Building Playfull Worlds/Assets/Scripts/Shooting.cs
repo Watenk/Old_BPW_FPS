cbc802bb47cc48f8befee6503dfdfff8e3365dda
Ivar Nuij <Ivar.nuij@gmail.com>
2022-03-07 14:39:49 +0100

    Added Buildings, Added Shotgun

--- Unity Building Playfull Worlds/Assets/Scripts/Shooting.cs
+++ Unity Building Playfull Worlds/Assets/Scripts/Shooting.cs
@@ -6,27 +6,74 @@ public class Shooting : MonoBehaviour
 {
     public Camera MainCam;
 
-    public float Damage = 20f;
-    public float Range = 100f;
+    //Weapons
+
+    public string EquipedWeapon = "Pistol";
+
+    //Pistol
+    public float PistolAmmo = 6f;
+    public float PistolTotalAmmo = 120f;
+    float PistolDamage = 20f;
+    float PistolRange = 100f;
+
+    //Shotgun
+    public float ShotgunAmmo = 2f;
+    public float ShotgunTotalAmmo = 24f;
+    float ShotgunDamage = 100f;
+    float ShotgunRange = 20f;
+
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        //Weapons
+        //Pistol
+        if (Input.GetButtonDown("Fire1") && EquipedWeapon == "Pistol")
         {
-            Shoot();
+            Shoot(PistolAmmo, PistolDamage, PistolRange);
+            PistolAmmo = CalcAmmo(PistolAmmo);
+        }
+
+        //Shotgun
+        if (Input.GetButtonDown("Fire1") && EquipedWeapon == "Shotgun") 
+        {
+            Shoot(ShotgunAmmo, ShotgunDamage, ShotgunRange);
+            ShotgunAmmo = CalcAmmo(ShotgunAmmo); 
+        }
+
+        //WeaponSwitch
+        if (Input.GetKeyDown("1"))
+        {
+            EquipedWeapon = "Pistol";
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            EquipedWeapon = "Shotgun";
         }
     }
 
-    void Shoot()
+    void Shoot(float AmmoAmount, float DamageAmount, float RangeAmount)
     {
-        RaycastHit Hit;
-        if (Physics.Raycast(MainCam.transform.position, MainCam.transform.forward, out Hit, Range))
+        if (AmmoAmount >= 1)
         {
-            Enemy Enemy = Hit.transform.GetComponent<Enemy>();
-
-            if (Enemy != null)
+            RaycastHit Hit;
+            if (Physics.Raycast(MainCam.transform.position, MainCam.transform.forward, out Hit, RangeAmount))
             {
-                Enemy.TakeDamage(Damage);
+                Enemy Enemy = Hit.transform.GetComponent<Enemy>();
+
+                if (Enemy != null)
+                {
+                    Enemy.TakeDamage(DamageAmount);
+                }
             }
         }
+    }
+
+    float CalcAmmo(float AmmoAmount)
+    {
+        if (AmmoAmount >= 1f)
+        {
+            AmmoAmount -= 1f;
+        }
+        return AmmoAmount;
     }
 }
--- Unity Building Playfull Worlds/Assets/Scripts/UI.cs
+++ Unity Building Playfull Worlds/Assets/Scripts/UI.cs
@@ -6,24 +6,32 @@ using UnityEngine.UI;
 public class UI : MonoBehaviour
 {
     private CharacterControlScript Player;
+    private Shooting ShootingScript;
 
     //JumpCoolDown
     public Slider JumpCooldownSlider;
 
     //Health
     public Slider HealthSlider;
+
+    //Ammo
+    public Text CurrentAmmo;
+    public Text TotalAmmo;
+
     
     //--------------------------------------------//
 
     void Start()
     {
         Player = FindObjectOfType<CharacterControlScript>();
+        ShootingScript = FindObjectOfType<Shooting>();
     }
 
     void Update()
     {
         JumpCoolDown();
         Health();
+        CalcAmmo();
     }
 
     //----------------------------------------------------//
@@ -36,5 +44,20 @@ public class UI : MonoBehaviour
     void Health()
     {
         HealthSlider.value = Player.PlayerHealth;
+    }
+
+    void CalcAmmo()
+    {
+        if (ShootingScript.EquipedWeapon == "Pistol")
+        {
+            CurrentAmmo.text = ShootingScript.PistolAmmo.ToString() + "/6";
+            TotalAmmo.text = ShootingScript.PistolTotalAmmo.ToString();
+        }
+
+        if (ShootingScript.EquipedWeapon == "Shotgun")
+        {
+            CurrentAmmo.text = ShootingScript.ShotgunAmmo.ToString() + "/2";
+            TotalAmmo.text = ShootingScript.ShotgunTotalAmmo.ToString();
+        }
     }
 }
